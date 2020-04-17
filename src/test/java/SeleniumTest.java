73b1f31cc9d5245db6b26a5d5bb2263abc3b1e23
DanilKozlov00 <55684917+DanilKozlov00@users.noreply.github.com>
2020-04-17 19:17:19 +0300

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+
 import java.util.Arrays;
 import java.util.Collection;
 
