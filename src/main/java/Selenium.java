f12fa049cdaa8e42d542beb5cbc73b13efd91175
DanilKozlov00 <55684917+DanilKozlov00@users.noreply.github.com>
2020-04-18 19:53:02 +0300

    Update Selenium.java

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -38,7 +38,6 @@ public class Selenium {
         options.addArguments("start-maximized");
         options.addArguments("enable-automation");
         options.addArguments("--no-sandbox");
-      
         options.addArguments("--disable-infobars");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-browser-side-navigation");
