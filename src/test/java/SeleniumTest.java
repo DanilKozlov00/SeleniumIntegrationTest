ae4233083e2b83735994c271e4206b89d5cabe58
DanilKozlov00 <55684917+DanilKozlov00@users.noreply.github.com>
2020-04-22 12:06:10 +0300

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -37,21 +37,21 @@ public class SeleniumTest {
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
 
-                {"https://github.com/vvtatyana/Losiash","1", "0","Файл Readme неверен" + "Наполнение файлов в ветке master неверно" + "Надпись на странице Home: 'Welcome to the Losyash wiki!'" + "а должно быть: 'Welcome to the Losiash wiki!'"},
-                {"https://github.com/vvtatyana/Krosh","2","0", "Неверное имя ветки"},
-                {"https://github.com/vvtatyana/Yozhik","3","0","Файл Readme не найден" + "В ветке master неверное кол-во файлов"},
-                {"https://github.com/vvtatyana/Barash","4","0","В ветке master неверное кол-во файлов"},
-                {"https://github.com/vvtatyana/Nusha","5","0","Имя labels не совпадает" + "Имя открытого pull_request неверно,либо не назначен label" +"Имя закрытого issues неверно"},
-                {"https://github.com/vvtatyana/Pin","6","0","Имя milestone некорректно"},
-                {"https://github.com/vvtatyana/Sovunya","7","0","Неверное имя project"},
-                {"https://github.com/vvtatyana/Pandi","8","0","Доска To do не содержит задачи: Внешность" + "Доска In progress не содержит задачи: Предпочтения и интересы" + "Доска Done не содержит задачи: Характер" + "Неверное имя открытого issue/issues"},
-                {"https://github.com/vvtatyana/Kar-Karych","9","0","Кол-во pull_request неверно" + "Не совпадает кол-во issues" + "В ветке master неверное кол-во файлов"},
-                {"https://github.com/vvtatyana/Kopatych","10","0","Имя открытого pull_request неверно,либо не назначен label" + "Неверное имя открытого issue/issues" + "В ветке master неверные файлы"},
-                {"https://github.com/vvtatyana/Bibi","11","0","Доска To do не содержит задачи: Отношения с другими смешариками" + "Доска Done не содержит задачи: Биография" + "Имя закрытого pull_request неверно,либо не назначен label" + "Неверное имя открытого issue/issues\n" + "В ветке master неверные файлы"},
-                {"https://github.com/vvtatyana/Mysharik","12","0","Кол-во pull_request неверно" + "Не совпадает кол-во issues" + "Неверное кол-во branches"},
-                {"https://github.com/vvtatyana/Mulya", "13", "0","Label верен, но назначен не на все задачи или/и pull_requests" + "Кол-во pull_request неверно" + "Не совпадает кол-во issues" + "В ветке master неверное кол-во файлов"},
-                {"https://github.com/vvtatyana/Zheleznaya_Nyanya", "14", "0","Label не назначен на задачи или или/и pull_requests " + "Доска To do не содержит задач." + "Доска In progress не содержит задач." + "Доска Done не содержит задач." + "Кол-во pull_request неверно\n" + "Не совпадает кол-во issues\n" + "В ветке master неверное кол-во файлов"},
-                {"https://github.com/vvtatyana/Tigritsiya", "15", "0","Кол-во pull_request неверно" + "Не совпадает кол-во issues" + "Неверное кол-во branches"}
+                {"https://github.com/vvtatyana/Losiash","1", "0","\"Файл Readme неверенНаполнение файлов в ветке master неверноНадпись на странице Home: 'Welcome to the Losyash wiki!'а должно быть: 'Welcome to the Losiash wiki!'\""},
+                {"https://github.com/vvtatyana/Krosh","2","0", "Неверное имя ветки\n"},
+                {"https://github.com/vvtatyana/Yozhik","3","0","Файл Readme не найден\nВ ветке master неверное кол-во файлов\n"},
+                {"https://github.com/vvtatyana/Barash","4","0","В ветке master неверное кол-во файлов\n"},
+                {"https://github.com/vvtatyana/Nusha","5","0", "Имя labels не совпадает\nИмя открытого pull_request неверно,либо не назначен label\nИмя закрытого issues неверно\n"},
+                {"https://github.com/vvtatyana/Pin","6","0","Имя milestone некорректно\n"},
+                {"https://github.com/vvtatyana/Sovunya","7","0","Неверное имя project\n"},
+                {"https://github.com/vvtatyana/Pandi","8","0","Доска To do не содержит задачи: Внешность\nДоска In progress не содержит задачи: Предпочтения и интересы\nДоска Done не содержит задачи: Характер\nНеверное имя открытого issue/issues\n"},
+                {"https://github.com/vvtatyana/Kar-Karych","9","0","Кол-во pull_request неверно\nНе совпадает кол-во issues\nВ ветке master неверное кол-во файлов\n"},
+                {"https://github.com/vvtatyana/Kopatych","10","0","Имя открытого pull_request неверно,либо не назначен label\nНеверное имя открытого issue/issues\nВ ветке master неверные файлы\n"},
+                {"https://github.com/vvtatyana/Bibi","11","0","Доска To do не содержит задачи: Отношения с другими смешариками\nДоска Done не содержит задачи: Биография\nИмя закрытого pull_request неверно,либо не назначен label\nНеверное имя открытого issue/issues\nВ ветке master неверные файлы\n"},
+                {"https://github.com/vvtatyana/Mysharik","12","0","Кол-во pull_request неверно\nНе совпадает кол-во issues\nНеверное кол-во branches\n"},
+                {"https://github.com/vvtatyana/Mulya", "13", "0","Label верен, но назначен не на все задачи или/и pull_requests\nКол-во pull_request неверно\nНе совпадает кол-во issues\nВ ветке master неверное кол-во файлов\n"},
+                {"https://github.com/vvtatyana/Zheleznaya_Nyanya", "14", "0","Label не назначен на задачи или или/и pull_requests \nДоска To do не содержит задач.\nДоска In progress не содержит задач.\nДоска Done не содержит задач.\nКол-во pull_request неверно\nНе совпадает кол-во issues\nВ ветке master неверное кол-во файлов\n" },
+                {"https://github.com/vvtatyana/Tigritsiya", "15", "0","Кол-во pull_request неверно\nНе совпадает кол-во issues\nНеверное кол-во branches\n"}
         });
     }
 
