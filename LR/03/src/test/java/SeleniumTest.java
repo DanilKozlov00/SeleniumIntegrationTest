90d740443dbb19cd195f60efddb7a0d030d383e0
DanilKozlov00 <55684917+DanilKozlov00@users.noreply.github.com>
2020-04-29 16:30:59 +0300

    123

--- LR/03/src/test/java/SeleniumTest.java
+++ LR/03/src/test/java/SeleniumTest.java
@@ -29,6 +29,7 @@ public class SeleniumTest {
         this.expected=expected;
         this.expectedrez=expectedrez;
         selen=new Selenium(url,number);
+
     }
 
     // name attribute is optional, provide an unique name for test
@@ -36,8 +37,7 @@ public class SeleniumTest {
     @Parameters(name = "{index}: ({0},{1}) = {2}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-
-                {"https://github.com/vvtatyana/Losiash","1", "0","Файл Readme неверен\nНаполнение файлов в ветке master неверно\nНадпись на странице Home: 'Welcome to the Losyash wiki!'\nа должно быть: 'Welcome to the Losiash wiki!'\n"},
+                {"https://github.com/vvtatyana/Losiash","1", "0","Неверное имя репозитория\n"},
                 {"https://github.com/vvtatyana/Krosh","2","0", "Неверное имя ветки\n"},
                 {"https://github.com/vvtatyana/Yozhik","3","0","Файл Readme не найден\nВ ветке master неверное кол-во файлов\n"},
                 {"https://github.com/vvtatyana/Barash","4","0","В ветке master неверное кол-во файлов\n"},
@@ -49,9 +49,10 @@ public class SeleniumTest {
                 {"https://github.com/vvtatyana/Kopatych","10","0","Имя открытого pull_request неверно,либо не назначен label\nНеверное имя открытого issue/issues\nВ ветке master неверные файлы\n"},
                 {"https://github.com/vvtatyana/Bibi","11","0","Доска To do не содержит задачи: Отношения с другими смешариками\nДоска Done не содержит задачи: Биография\nИмя закрытого pull_request неверно,либо не назначен label\nНеверное имя открытого issue/issues\nВ ветке master неверные файлы\n"},
                 {"https://github.com/vvtatyana/Mysharik","12","0","Кол-во pull_request неверно\nНе совпадает кол-во issues\nНеверное кол-во branches\n"},
-                {"https://github.com/vvtatyana/Mulya", "13", "0","Label верен, но назначен не на все задачи или/и pull_requests\nКол-во pull_request неверно\nНе совпадает кол-во issues\nВ ветке master неверное кол-во файлов\n"},
+                {"https://github.com/vvtatyana/Mulya", "13", "0","Label верен, но назначен не на все задачи или/и pull_requests\nОшибка в pull_requests. Должно быть 1 открытый, 1 закрытый\nНе совпадает кол-во issues\nВ ветке master неверное кол-во файлов\n"},
                 {"https://github.com/vvtatyana/Zheleznaya_Nyanya", "14", "0","Label не назначен на задачи или или/и pull_requests \nДоска To do не содержит задач.\nДоска In progress не содержит задач.\nДоска Done не содержит задач.\nКол-во pull_request неверно\nНе совпадает кол-во issues\nВ ветке master неверное кол-во файлов\n" },
-                {"https://github.com/vvtatyana/Tigritsiya", "15", "0","Кол-во pull_request неверно\nНе совпадает кол-во issues\nНеверное кол-во branches\n"}
+                {"https://github.com/vvtatyana/Tigritsiya", "15", "0","Кол-во pull_request неверно\nНе совпадает кол-во issues\nНеверное кол-во branches\n"},
+                {"https://github.com/HozookiSan/Tigritsiya","15","1","Работа сдана\n"},
         });
     }
 
